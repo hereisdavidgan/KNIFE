d99e0b611d95a54c3f2ffab4623775eda4051be6
hereisdavidgan <571825401@qq.com>
2021-09-09 00:25:34 +0800

    still confuce

--- hundred/10Wednesday.py
+++ hundred/10Wednesday.py
@@ -30,16 +30,16 @@
 """69 有n个人围成一圈，顺序排号。
 从第一个人开始报数（从1到3报数），凡报到3的人退出圈子，问最后留下的是原来第几号的那位。"""
 # data = [i+1 for i in range(20)]
-data = [12, 11, 4, 2, 0, 1, 123]
-print(data)
-i = 1 
-while len(data) > 1:
-    if i % 3 == 0:
-        data.pop(0)
-    else:
-        data.insert(len(data),data.pop(0))
-        print(data)
-    i += 1
-print(data)
+# data = [12, 11, 4, 2, 0, 1, 123]
+# print(data)
+# i = 1 
+# while len(data) > 1:
+#     if i % 3 == 0:
+#         data.pop(0)
+#     else:
+#         data.insert(len(data),data.pop(0))
+#         print(data)
+#     i += 1
+# print(data)
 
-
+        
