f8cec3b91b06b42585f6e88ba9e950323b88c358
hereisdavidgan <571825401@qq.com>
2021-09-12 22:08:09 +0800

    leetcode

---
+++ leetcode/DATA STRUCTURE/01Sunday.py
@@ -0,0 +1,15 @@
+""" ## 217. 存在重复元素
+给定一个整数数组，判断是否存在重复元素。
+如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false """
+""" 
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i]== nums[i+1]:
+                return True
+        return False
+print(Solution.containsDuplicate(1,[1,1,2,3]))
+print(Solution.containsDuplicate(1,[1,4,2,3]))
+print(Solution.containsDuplicate(1,[1,1,2,2,3,3])) 
+"""
---
+++ leetcode/readme.md
@@ -0,0 +1,3 @@
+## data struture
+start from 2021/9/12
+planing to be completed in two weeks
