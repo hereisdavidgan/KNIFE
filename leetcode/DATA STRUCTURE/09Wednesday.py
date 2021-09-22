a9b47bbe54c70f69f6548bbc620f66d75a7c9668
hereisdavidgan <571825401@qq.com>
2021-09-22 23:07:58 +0800

    123

---
+++ leetcode/DATA STRUCTURE/09Wednesday.py
@@ -0,0 +1,36 @@
+ #########################################################
+"""206. 反转链表"""
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre, cur = None, head
+        while cur:
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur = nxt
+        return pre
+ #########################################################
+"""206. 反转链表"""
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
+ #########################################################
+"""83. 删除排序链表中的重复元素"""
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        cur = head
+        while cur.next:
+            if cur.val == cur.next.val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return head
