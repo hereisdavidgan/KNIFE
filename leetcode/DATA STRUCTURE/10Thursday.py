32bfe738e4d77ac5dd2c7245b946538c78f77ea9
hereisdavidgan <571825401@qq.com>
2021-09-23 23:17:35 +0800

    zhan

---
+++ leetcode/DATA STRUCTURE/10Thursday.py
@@ -0,0 +1,39 @@
+#########################################################
+"""20. 有效的括号"""
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 == 1:
+            return False
+        
+        pairs = {
+            ")": "(",
+            "]": "[",
+            "}": "{",
+        }
+        stack = list()
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+            else:
+                stack.append(ch)
+        return not stack
+
+
+#########################################################
+"""20. 有效的括号"""
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic: stack.append(c)
+            elif dic[stack.pop()] != c: return False 
+        return len(stack) == 1
+
+
+
+#########################################################
+"""20. 有效的括号"""
