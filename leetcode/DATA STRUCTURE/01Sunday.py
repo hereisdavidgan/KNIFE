a4223bdcf879244f4079e91e44526a6129a52d59
hereisdavidgan <571825401@qq.com>
2021-09-28 22:13:47 +0800

    da

--- leetcode/DATA STRUCTURE/01Sunday.py
+++ leetcode/DATA STRUCTURE/01Sunday.py
@@ -33,4 +33,11 @@ class Solution:
                 single = 0
         return maxs
 
-
+## 动态规划
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(1,n):
+            if nums[i-1]>0:
+                nums[i]+=nums[i-1]
+        return max(nums)
